fd11d2c3a1c51be430d1e7b97edba8692e937073
OmkarShidore <omkarshidore@gmail.com>
2022-01-07 13:06:42 +0530

    fet: rgb to grayscale

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import cv2
+
 def sum(num1, num2):
     return num1+num2
 
@@ -6,3 +8,12 @@ def prod(num1, num2):
 
 def sub(num1, num2):
     return num1-num2
+
+image = cv2.imread('C:/Users/N/Desktop/Test.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  
+cv2.imshow('Original image',image)
+cv2.imshow('Gray image', gray)
+  
+cv2.waitKey(0)
+cv2.destroyAllWindows()
